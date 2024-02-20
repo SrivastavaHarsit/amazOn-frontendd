a78fc3463b2f7698193ef5353a7067b8e0cf695d
SrivastavaHarshit <srivastavaharshit1007@gmail.com>
2024-02-21 00:26:42 +0530

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import {products, getProduct} from '../../data/products.js'
 import formatCurrency  from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js'
+import { renderPaymentSummary } from './paymentSummary.js';
 
 
 export function renderOrderSummary () {
@@ -130,6 +131,7 @@ document.querySelectorAll('.js-delete-link')
           `.js-cart-item-container-${productId}`
         )
         container.remove();
+        renderPaymentSummary();
     });
  });
 
@@ -140,6 +142,7 @@ document.querySelectorAll('.js-delete-link')
       const { productId, deliveryOptionId } = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
      });
    });
 }
