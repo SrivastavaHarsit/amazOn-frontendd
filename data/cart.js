5139d067f436859f3fa8f7cdd0fc6df9c1d3c9c3
SrivastavaHarshit <srivastavaharshit1007@gmail.com>
2024-02-20 21:45:40 +0530

    save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,18 @@
 // basically cart jo bhi add hoga wo 1 array of objects hoga
+//localStorage me string me hoga store toh usko apna array of objects banane ke liyte JSON.parse() use kar rhe apan
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
 //agar localStorage me kuch ni hai toh default value
 if(!cart) {
     cart = [{
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 2
+        quantity: 2,
+        deliveryOptionId: '1'
     },
     {
         productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 1
+        quantity: 1,
+        deliveryOptionId: '1'
     }];
     
 }
@@ -35,7 +38,8 @@ export function addToCart(productId) {
     else {
      cart.push({
          productId,
-         quantity: 1
+         quantity: 1,
+         deliveryOptionId: '1' //default is free wala delivery option
         });
     }
     saveToStorage();
